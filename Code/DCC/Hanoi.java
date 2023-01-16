80bbe857f29275549029fdf6887590697456ba0b
DURUII <47244932+DURUII@users.noreply.github.com>
2023-01-16 18:19:12 +0800

    Hanoi🥏

--- Code/DCC/Hanoi.java
+++ Code/DCC/Hanoi.java
@@ -3,19 +3,29 @@ package DCC;
 
 public class Hanoi {
     /**
-     * <a href="https://mathsisfun.com/games/towerofhanoi.html">汉诺塔</a>
+     * <a href="https://mathsisfun.com/games/towerofhanoi.html">线上游戏</a>
      */
     public static void main(String[] args) {
-        move(3, 1, 3, 2);
+        // T(n) = 2^n -1 = O(2^n)
+        move(3, "A", "B", "C");
     }
 
-    // 将 src 柱上 n 个盘片，转移至 dest 柱（可以借助空闲的 spare 柱）
-    static void move(int n, int src, int dest, int spare) {
+    /**
+     * 将 src 柱上 n 个盘片，借助 spare 柱，转移至 dest 柱
+     */
+    static void move(int n, String src, String dest, String spare) {
+        // base case
         if (n == 1) {
-            System.out.println(src + "->" + dest);
-        } else {
+            System.out.println(src + " -> " + dest);
+        }
+
+        // recursive case
+        else {
+            // Move Disk 1 ~ n-1 from A to B
             move(n - 1, src, spare, dest);
-            move(1, src, dest, -1);
+            // Move Disk No. n from A to C
+            move(1, src, dest, "");
+            // Move Disk 1 ~ n-1 from B to C
             move(n - 1, spare, dest, src);
         }
     }
