9c5354ffa7a0d6c094c8d817427102abf2bda0a7
DURUII <47244932+DURUII@users.noreply.github.com>
2023-01-18 10:17:16 +0800

    🐰

--- Code/DCC/Fibonacci.java
+++ Code/DCC/Fibonacci.java
@@ -3,23 +3,18 @@ package DCC;
 import java.util.Scanner;
 
 public class Fibonacci {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            int n = sc.nextInt();
-            if (n >= 0) {
-                System.out.println(fib(n));
-            }
-        }
+        int n = sc.nextInt();
+        if (n >= 0) System.out.println(fib(n));
     }
 
     /**
      * Recurrence
      */
-    static int fib(int n) {
+    public static int fib(int n) {
         // base case: directly output
-        if (n < 2) return 1;
+        if (n < 2) return n == 0 ? 0 : 1;
         // recursive case: divide the problem into two sub-problems
         // compute each sub-problem recursively and add them both
         return fib(n - 1) + fib(n - 2);
