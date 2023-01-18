fd646c85e318e8d1c19e6e90a31aba5c48501099
DURUII <47244932+DURUII@users.noreply.github.com>
2023-01-18 18:30:58 +0800

    🃏

--- Code/DCC/K_Selection.java
+++ Code/DCC/K_Selection.java
@@ -3,8 +3,8 @@ package DCC;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
-// https://leetcode.com/problems/kth-largest-element-in-an-array/
 public class K_Selection {
     static int[] a = new int[]{3, 2, 3, 1, 2};
 
@@ -24,21 +24,24 @@ public class K_Selection {
         // COMMENTS: SOOOOOOOOOOO MANY TRIVIAL DETAILS
         int remainder = nums.length % 5;
         int numOfGroup = (remainder > 0) ? (nums.length / 5 + 1) : (nums.length / 5);
-        int[] m = new int[numOfGroup];
+        int[] median = new int[numOfGroup];
         int index = 0;
         if (remainder != 0) {
             int res = nums[remainder / 2];
             if ((remainder & 1) != 1) res = (res + nums[remainder / 2 + 1]) / 2;
-            m[numOfGroup - 1] = res;
+            median[numOfGroup - 1] = res;
             index = remainder - 1;
         }
         for (; index + 4 < nums.length; index += 5) {
             Arrays.sort(nums, index, index + 5);
-            m[index / 5] = nums[index + 2];
+            median[index / 5] = nums[index + 2];
         }
 
         // FIXME theoretically best pivot (median of medians)
-        int MoM = findKthLargest(m, m.length / 2 > 0 ? m.length / 2 : m.length / 2 + 1);
+        int MoM = findKthLargest(median, median.length / 2 > 0 ? median.length / 2 : median.length / 2 + 1);
+
+        // FIXME randomized median is of competitive edge on LEETCODE
+        // int MoM = nums[new Random().nextInt(nums.length)];
 
         List<Integer> less = new ArrayList<>();
         List<Integer> more = new ArrayList<>();
@@ -49,21 +52,18 @@ public class K_Selection {
             else equal.add(nums[i]);
         }
 
-        // FIXME Selection in X<, X> or X=
+
+        // FIXME Which to conquer: X<, X> or X= ?
         if (k - 1 < more.size()) {
             int[] moreArray = new int[more.size()];
-            for (int i = 0; i < more.size(); i++) {
-                moreArray[i] = more.get(i);
-            }
+            for (int i = 0; i < more.size(); i++) moreArray[i] = more.get(i);
             return findKthLargest(moreArray, k);
         } else if (k - 1 < more.size() + equal.size()) {
             return MoM;
         }
-
+        
         int[] lessArray = new int[less.size()];
-        for (int i = 0; i < less.size(); i++) {
-            lessArray[i] = less.get(i);
-        }
+        for (int i = 0; i < less.size(); i++) lessArray[i] = less.get(i);
         return findKthLargest(lessArray, k - more.size() - equal.size());
     }
 }
